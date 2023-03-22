b518402dc8c3e2cae11d30bc9fa2e5cbe420eb30
mohiniprasad999 <mohini.prasad@stackinfinite.com>
2023-03-22 16:15:46 +0530

    todo api

---
+++ src/model/todo.model.ts
@@ -0,0 +1,19 @@
+// import mongoose from 'mongoose';
+
+// const todos: Todo[] = [];
+
+// interface Todo {
+//   id: string;
+//   title: string;
+//   description: string,
+//   completed: boolean;
+// }
+
+// const todoSchema = new mongoose.Schema<Todo>({
+//     title: { type: String, required: true },
+//     description: { type: String, required: true},
+//     completed: { type: Boolean, default: false },
+//   });
+  
+
+// const TodoModel = mongoose.model<Todo>('Todo', todoSchema);
